d48893467f61672df5e72b0aac70ac97bebcb3b9
Rudraksh Naik <55753415+rud-rax@users.noreply.github.com>
2022-09-13 19:26:29 +0530

    Change Directories for testing

--- test/crop_image.py
+++ test/crop_image.py
@@ -9,17 +9,17 @@ COMPAREFILE = ""
 
 img1 = convert_from_path(MAINFILE)
 for i in range(len(img1)):
-    img1[i].save(f"industrySample/page{i}.jpg", "JPEG")
+    img1[i].save(f"industrySample/page1{i}.jpg", "JPEG")
 
 
 IMAGEPATH = r"industrySample/page0.jpg"
 
-img1 = ImageObj(IMAGEPATH)
-print(img1.checkImage())
-img1.showImage()
-print(img1.getResolution())
+# img1 = ImageObj(IMAGEPATH)
+# print(img1.checkImage())
+# img1.showImage()
+# print(img1.getResolution())
 
 
-sift = cv2.xfeatures2d.SIFT
+# sift = cv2.xfeatures2d.SIFT
 
-cv2.SIFT_create()
+# cv2.SIFT_create()
